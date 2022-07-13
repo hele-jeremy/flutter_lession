bfd3ae79e5ea145c044b25e82325d90219089877
hoko <hele@xgd.com>
2022-07-13 21:08:06 +0800

    Dart中的异步操作: isolates/Event Loops Future   错误处理 Stream async/await

--- lib/dart_lesson/asynchrony/dart_异步.dart
+++ lib/dart_lesson/asynchrony/dart_异步.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_lesson/utils/log_utils.dart';
 
@@ -23,7 +25,9 @@ class DartAsync extends StatelessWidget {
   void _dartAsync() {
     // _test1();
     // _test2();
-    _test3();
+    // _test3();
+    // _test4();
+    _test5();
   }
 }
 
@@ -54,6 +58,9 @@ String createOrderMessage() {
 /// 要定义一个async函数，请在函数体之前添加async:
 /// await关键字仅在异步函数中有效。
 
+/// await 表达式的返回值通常是一个 Future 对象；如果不是的话也会自动将其包裹在一个 Future 对象里。
+/// Future 对象代表一个“承诺”， await 表达式会阻塞直到需要的对象返回。
+
 void _test1() {
   ///打印的结果不符合我们的预期:Your order is Large latte
   LogUtils.d(createOrderMessage()); //Your order is Instance of 'Future<String>'
@@ -80,15 +87,19 @@ void _test2() async {
 ///async函数会同步(sync)运行，直到执行到第一个await关键字。
 ///这意味着在异步(async)函数体中，在第一个await关键字之前的所有同步(sync)代码都会立即执行。
 
-Future<String> fetchUserOrder3() =>
-    // Future.delayed(const Duration(seconds: 4), () => "large latte!");
-    ///也可以抛出异常
-    Future.delayed(
-        const Duration(seconds: 4), () => throw "Cannot locate uesr order!!!");
+Future<String> fetchUserOrder3() {
+  LogUtils.d("excute fetchUserOrder3....!");
+  return Future.delayed(const Duration(seconds: 4), () => "large latte!");
+
+  ///也可以抛出异常
+  // Future.delayed(const Duration(seconds: 4), () => throw "Cannot locate uesr order!!!");
+}
 
 Future<void> printOrderMessage() async {
   ///在异步函数中，可以像在同步代码中一样编写try-catch子句。
   try {
+    ///async函数体中的Future结果必须使用await来等待Future结果或标记为unawaited显式的忽略结果
+    unawaited(fetchUserOrder3());
     var userOrder = await fetchUserOrder3();
     LogUtils.d("Awaiting user order....");
     LogUtils.d("Your order is $userOrder");
@@ -107,4 +118,68 @@ void _test3() async {
   LogUtils.d("-----------------------------------------------");
   _countSeconds(4);
   await printOrderMessage();
+}
+
+///Future的使用和错误处理
+Future<int> myFunc() {
+  LogUtils.d("myFunc execute....!");
+
+  ///抛出异常
+  return Future.delayed(const Duration(seconds: 2),
+      () => throw ArgumentError("myFunc argument illegal...!"));
+
+  // return Future.delayed(const Duration(seconds: 2), () => Future.value(100));
+}
+
+void _test4() {
+  LogUtils.d("_test4--------------------------");
+  myFunc().then((value) {
+    LogUtils.d("then value = $value");
+
+    ///then中接收处理了myFunc的成功值(completed value),然后then中抛出了一个异常
+    // throw Exception("then process error....!");
+  }, onError: (err, stackTrace) {
+    //一般不建议注册多个错误处理回调
+    ///then中注册了一个error错误处理回调,然后then的onError错误处理回调，先自己处理了myFunc的error
+    LogUtils.d("then handle error...!");
+
+    ///然后再重新抛出了一个异常,这个异常被下游的catchError处理
+    throw Exception("then onError throw another error...!");
+  }).catchError((err, stackTrace) {
+    LogUtils.d("catchError .....!");
+    LogUtils.d("err:$err\n$stackTrace");
+  });
+}
+
+Future<String> one() {
+  LogUtils.d("one excute...!");
+  return Future.value("from one");
+}
+
+Future<String> two() {
+  LogUtils.d("two excute...!");
+  return Future.error("error from two");
+}
+
+Future<String> three() {
+  LogUtils.d("three excute....!");
+  return Future.value("from three");
+}
+
+Future<String> four() {
+  LogUtils.d("four excute...!");
+  return Future.value("from four");
+}
+
+void _test5() {
+  one() //以一个值完成(completed a value)
+      .then((_) => two()) //以一个错误完成(complete a error)
+      .then((_) => three()) //以two的error完成(completed a error form two)
+      .then((_) => four()) //以two的error完成(completed a error form two)
+      .then((value) => value.length) //以two的error完成(completed a error form two)
+      .catchError((err, stackTrace) {
+    LogUtils.d("Got error:$err"); //捕获了two的error
+    ///重新以42完成(Future completed with a value)
+    return 42;
+  }).then((value) => LogUtils.d("The value is $value"));
 }
